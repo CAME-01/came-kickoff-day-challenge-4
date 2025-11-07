cf5e2e8b6417b3b28d4092cc8c59729e7aba5c82
Pubudu Janith <pubudujanith123@gmail.com>
2025-11-07 22:09:15 +0530

    Add fetch method to service layer

--- src/main/java/lk/pubudu/app/service/PersonService.java
+++ src/main/java/lk/pubudu/app/service/PersonService.java
@@ -1,9 +1,37 @@
 package lk.pubudu.app.service;
 
+import jakarta.annotation.PostConstruct;
+import lk.pubudu.app.dto.PersonDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PersonService {
+
+    private static final String URL = "https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json";
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Initial fetch on startup...");
+        fetchJsonData();
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void fetchJsonData() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<PersonDTO[]> response = restTemplate.getForEntity(URL, PersonDTO[].class);
+        List<PersonDTO> persons = Arrays.asList(response.getBody());
+
+        System.out.println("Fetched " + persons.size() + " persons");
+        persons.stream().limit(3).forEach(System.out::println);
+    }
+
 }
