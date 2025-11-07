dcfbd300ded36bb9f09b5140f952ab4af55ea422
Pubudu Janith <pubudujanith123@gmail.com>
2025-11-07 21:46:28 +0530

    Implement DTO class

--- src/main/java/lk/pubudu/app/dto/PersonDTO.java
+++ src/main/java/lk/pubudu/app/dto/PersonDTO.java
@@ -1,4 +1,21 @@
 package lk.pubudu.app.dto;
 
-public class PersonDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1099389437728308115L;
+    private String id;
+    private String name;
+    private String language;
+    private String bio;
+    private Double version;
 }
