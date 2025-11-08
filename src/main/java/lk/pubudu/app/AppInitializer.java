29d0e29dcf4232125ca1d08c3d759df5fb481675
Pubudu Janith <pubudujanith123@gmail.com>
2025-11-08 06:45:20 +0530

    Update service layer

--- src/main/java/lk/pubudu/app/AppInitializer.java
+++ src/main/java/lk/pubudu/app/AppInitializer.java
@@ -3,8 +3,10 @@ package lk.pubudu.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableScheduling
+@EnableTransactionManagement
 @SpringBootApplication
 public class AppInitializer {
 
--- src/main/java/lk/pubudu/app/service/PersonService.java
+++ src/main/java/lk/pubudu/app/service/PersonService.java
@@ -1,7 +1,11 @@
 package lk.pubudu.app.service;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import lk.pubudu.app.dto.PersonDTO;
+import lk.pubudu.app.entity.Person;
+import lk.pubudu.app.repository.PersonRepository;
+import lk.pubudu.app.util.Transformer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,22 +20,49 @@ import java.util.List;
 public class PersonService {
 
     private static final String URL = "https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json";
+    private final Transformer transformer;
+    private final PersonRepository personRepository;
 
     @PostConstruct
+    @Transactional(rollbackOn = Throwable.class)
     public void init() {
+        // Delete all existing records before inserting new ones
+        personRepository.deleteAll();
+        System.out.println("Cleared existing records in the database.");
+
         System.out.println("Initial fetch on startup...");
-        fetchJsonData();
+        List<PersonDTO> personDTOS = fetchJsonData();
+        persistToMySql(personDTOS);
     }
 
     @Scheduled(fixedRate = 5000)
-    public void fetchJsonData() {
+    public List<PersonDTO> fetchJsonData() {
         RestTemplate restTemplate = new RestTemplate();
 
         ResponseEntity<PersonDTO[]> response = restTemplate.getForEntity(URL, PersonDTO[].class);
+        assert response.getBody() != null;
         List<PersonDTO> persons = Arrays.asList(response.getBody());
 
         System.out.println("Fetched " + persons.size() + " persons");
+        // print first three records
         persons.stream().limit(3).forEach(System.out::println);
+
+        return persons;
+    }
+
+    @Transactional(rollbackOn = Throwable.class)
+    public void persistToMySql(List<PersonDTO> persons) {
+        // Delete all existing records before inserting new ones
+        personRepository.deleteAll();
+        System.out.println("Cleared existing records in the database.");
+
+        // Transform and save all new records
+        List<Person> personList = persons.stream()
+                .map(transformer::toPersonEntity)
+                .toList();
+
+        personRepository.saveAll(personList);
+        System.out.println("Inserted " + personList.size() + " new records into the database.");
     }
 
 }
